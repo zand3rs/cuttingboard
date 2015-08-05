1625d5cbb8e412769339e47c1cb58fb26aa5d078
zander <aamagtipon@voyagerinnovation.com>
2015-08-05 13:39:49 +0800

    style implementation

--- index.js
+++ index.js
@@ -28,16 +28,22 @@ function Cuttingboard(options) {
   self._storage = _options.storage || "file";
   self._folder = _options.folder || "";
   self._bucket = _options.bucket || "";
-  self._consumerKey = _options.consumerKey || "";
-  self._consumerSecret = _options.consumerSecret || "";
+  self._key = _options.key || "";
+  self._secret = _options.secret || "";
   self._baseUri = _options.baseUri || "";
 
+  Object.defineProperty(this, "styles", {
+    get: function() {
+      return self._styles;
+    }
+  });
 }
 
 //------------------------------------------------------------------------------
 
 Cuttingboard.prototype.style = function(name, attrs) {
   var self = this;
+  self._styles[name] = _.cloneDeep(attrs);
 }
 
 //------------------------------------------------------------------------------
--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -5,6 +5,27 @@ describe(TEST_NAME, function() {
   describe("constructor", function() {
     it("should be successful", function() {
       var board = new Cuttingboard();
+      expect(board).to.be.an("object");
+    });
+  });
+
+  describe("#styles", function() {
+    it("should have a default original style", function() {
+      var board = new Cuttingboard();
+      expect(board.styles).to.have.property("original")
+                          .that.is.an("object")
+                          .that.deep.equals({ process: "copy" });
+    });
+  });
+
+  describe("#style()", function() {
+    it("should merge the provided style to the styles setting", function() {
+      var board = new Cuttingboard();
+      var style = { size: "50x50", process: "crop", extension: "png" };
+      board.style("thumb", style);
+      expect(board.styles).to.have.property("thumb")
+                          .that.is.an("object")
+                          .that.deep.equals(style);
     });
   });
 
