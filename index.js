87cf5140237fa9ddb99f3be9553853f91e8ac089
zander <aamagtipon@voyagerinnovation.com>
2015-10-18 18:36:18 +0800

    WIP: initial implementation

--- index.js
+++ index.js
@@ -3,53 +3,121 @@
  *
  */
 
-//==============================================================================
-//-- export
-
 module.exports = Cuttingboard;
 
-//==============================================================================
-//-- require
 
 var _ = require("lodash");
 var async = require("async");
+var path = require("path");
+
+var processor = require(path.join(__dirname, "lib", "processor"));
+var helper = require(path.join(__dirname, "lib", "helper"));
+
+var validImageTypes = ["jpg", "png", "gif", "bmp"];
 
 //==============================================================================
 //-- constructor
 
 function Cuttingboard(options) {
   var self = this;
-  var _options = _.clone(options) || {};
+  var _options = _.merge({
+    storage: "file",
+    imageName: "image",
+    imageFormat: "",
+    baseUri: ""
+  }, options);
 
-  self._prefix = _options.prefix || "";
-  self._extension = _options.extension || "";
-  self._styles = _.merge({ original: { process: "copy" } }, _options.styles);
+  var _module = path.join(__dirname, "lib", "stores", _options.storage);
+  var _storeImpl = require(_module);
+  var _store = new _storeImpl(_options);
 
-  self._storage = _options.storage || "file";
-  self._folder = _options.folder || "";
-  self._bucket = _options.bucket || "";
-  self._key = _options.key || "";
-  self._secret = _options.secret || "";
-  self._baseUri = _options.baseUri || "";
+  var _defaultStyle = { original: { process: "copy" } };
+  var _styles = _.merge(_.cloneDeep(_defaultStyle), _options.styles);
 
-  Object.defineProperty(this, "styles", {
+  //----------------------------------------------------------------------------
+  //-- properties
+
+  Object.defineProperty(self, "options", {
     get: function() {
-      return self._styles;
+      return _options;
+    }
+  });
+
+  Object.defineProperty(self, "store", {
+    get: function() {
+      return _store;
+    }
+  });
+
+  Object.defineProperty(self, "styles", {
+    get: function() {
+      return _styles;
+    },
+    set: function(styles) {
+      _styles = _.merge(_.cloneDeep(_defaultStyle), styles);
     }
   });
 }
 
-//------------------------------------------------------------------------------
+//==============================================================================
+//-- public instance methods
 
 Cuttingboard.prototype.style = function(name, attrs) {
-  var self = this;
-  self._styles[name] = _.cloneDeep(attrs);
-}
+  this.styles[name] = _.cloneDeep(attrs);
+};
 
 //------------------------------------------------------------------------------
 
 Cuttingboard.prototype.process = function(options, done) {
   var self = this;
-}
+  var images = {};
+  var imagePath = options.imagePath;
+  var imageType = helper.imageType(imagePath);
+  var imageFormat = options.imageFormat || self.options.imageFormat || imageType;
+
+  if (!_.contains(validImageTypes, imageFormat)) {
+    return done(new Error("Invalid image format!"));
+  }
+  var fileName = (options.imageName || self.options.imageName) + "." + imageFormat;
+
+  function processImage(style, done) {
+    async.auto({
+      processedImage: function(next) {
+        var convert = processor[style.process];
+        if (!_.isFunction(convert)) {
+          return next();
+        }
+        var params = { format: imageFormat };
+        convert(params, next);
+      },
+      savedImage: ["processedImage", function(next, result) {
+        var processedImage = result.processedImage;
+        if (_.isEmpty(processedImage)) {
+          return next();
+        }
+
+        var params = { fileName: fileName, data: processedImage };
+        self.store.save(params, next);
+      }]
+    }, function(err, result) {
+      var savedImage = result.savedImage;
+      done(err, savedImage);
+    });
+  }
+
+  async.forEachOf(self.styles, function(style, styleName, next) {
+    if (!_.isObject(style)) {
+      return next();
+    }
+    processImage(_.merge({ name: styleName }, style), function(err, imagePath) {
+      if (!err && imagePath) {
+        images[styleName] = imagePath;
+      }
+      next(err);
+    });
+  }, function(err) {
+    done(err, images);
+  });
+};
 
 //==============================================================================
