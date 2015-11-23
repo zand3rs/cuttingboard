f4545262b591e07c1ab9c0f5888160f0366bfaf5
zander <aamagtipon@voyagerinnovation.com>
2015-11-23 17:43:58 +0800

    crop processor implementation

--- test/unit/lib/processor.test.js
+++ test/unit/lib/processor.test.js
@@ -2,8 +2,6 @@ require("node-test-helper");
 var fs = require("fs");
 
 describe(TEST_NAME, function() {
-
-  describe(".copy()", function() {
     var imageBuffer;
 
     before(function(done) {
@@ -13,6 +11,7 @@ describe(TEST_NAME, function() {
       });
     });
 
+  describe(".copy()", function() {
     it("should return a copied data buffer of the given source file", function(done) {
       processor.copy({ path: jpg_image }, function(err, data) {
         expect(err).to.not.exist;
@@ -37,6 +36,52 @@ describe(TEST_NAME, function() {
 
     it("accepts format parameter", function(done) {
       processor.copy({ src: jpg_image, format: "png" }, function(err, data) {
+        expect(err).to.not.exist;
+        expect(data).to.exist;
+        expect(data).to.be.instanceof(Buffer);
+        expect(helper.isPng(data)).to.be.true;
+        expect(data.toString() === imageBuffer.toString()).to.be.false;
+        done(err);
+      });
+    });
+  });
+
+  describe(".crop()", function() {
+    it("should return a cropped data buffer of the given source file", function(done) {
+      processor.crop({ path: jpg_image, size: "50x50" }, function(err, data) {
+        expect(err).to.not.exist;
+        expect(data).to.exist;
+        expect(data).to.be.instanceof(Buffer);
+        expect(helper.isJpg(data)).to.be.true;
+        expect(data.toString() === imageBuffer.toString()).to.be.false;
+        done(err);
+      });
+    });
+
+    it("accepts offset parameter", function(done) {
+      processor.crop({ src: jpg_image, size: "60x60", offset: "1,1" }, function(err, data) {
+        expect(err).to.not.exist;
+        expect(data).to.exist;
+        expect(data).to.be.instanceof(Buffer);
+        expect(helper.isJpg(data)).to.be.true;
+        expect(data.toString() === imageBuffer.toString()).to.be.false;
+        done(err);
+      });
+    });
+
+    it("accepts pos as offset parameter", function(done) {
+      processor.crop({ src: jpg_image, size: "60x60", pos: "2,2" }, function(err, data) {
+        expect(err).to.not.exist;
+        expect(data).to.exist;
+        expect(data).to.be.instanceof(Buffer);
+        expect(helper.isJpg(data)).to.be.true;
+        expect(data.toString() === imageBuffer.toString()).to.be.false;
+        done(err);
+      });
+    });
+
+    it("accepts format parameter", function(done) {
+      processor.crop({ src: jpg_image, size: "50x50", format: "png" }, function(err, data) {
         expect(err).to.not.exist;
         expect(data).to.exist;
         expect(data).to.be.instanceof(Buffer);
